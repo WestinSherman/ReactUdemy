e0909debcb6eee14332a7a83d2ade44bfa44941b
WestinSherman <westin@arnst.ca>
2025-02-01 13:57:36 +0100

    S_63(625), Finished.

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -2,16 +2,19 @@ import './App.css'
 import Chicken from './Chicken';
 import Greeter from './Greeter';
 import Die from './Die';
+import ListPicker from './ListPicker';
 
 function App() {
   return (
     <div>
-      <Greeter person="Bill" from="Timmy" />
+      <ListPicker values={[1, 2, 3, 4, 5]} />
+      <ListPicker values={{ a: 1, b: 2 }} />
+      {/*<Greeter person="Bill" from="Timmy" />
       <Greeter person="Ted" />
       <Greeter from="Anne" />
       <Die numSides={20} />
       <Die numSides={6} />
-      <Die numSides={10} />
+      <Die numSides={10} />*/}
     </div>
   );
 };
