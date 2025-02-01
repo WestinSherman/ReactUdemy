8a84af17decd5704203a0e49e15855ebf4c36187
WestinSherman <westin@arnst.ca>
2025-02-01 01:33:55 +0100

    Add initial project files

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Chicken from './Chicken';
+import Greeter from './Greeter';
+
+function App() {
+  return (
+    <div>
+      <Greeter person="Bill" />
+      <Greeter person="Ted" />
+      <Greeter person="Rosa" />
+    </div>
+  );
+};
+
+export default App
