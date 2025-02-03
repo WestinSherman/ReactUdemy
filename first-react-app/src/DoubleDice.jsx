086f3576d3b623114e69216577990a98bf37abf9
WestinSherman <westin@arnst.ca>
2025-02-03 15:40:17 +0100

    S_63(626), Finished.

--- first-react-app/src/DoubleDice.jsx
+++ first-react-app/src/DoubleDice.jsx
@@ -1,6 +1,14 @@
 export default function DoubleDice() {
     const num1 = Math.floor(Math.random() * 3) + 1;
     const num2 = Math.floor(Math.random() * 3) + 1;
+    return (
+        <div>
+            <h2>{num1 === num2 ? "You Win!" : "You Lose :("}</h2>
+            {num1 === num2 ? <p>🎉🎉🎉</p> : <p>😥😥😥</p>}
+            <p>Num1: {num1}</p>
+            <p>Num2: {num2}</p>
+        </div>
+    );
 }
 
 
