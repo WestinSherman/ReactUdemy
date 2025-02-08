07a7c82ae997d8e7b67a5e70caeea6f6a15f5496
WestinSherman <westin@arnst.ca>
2025-02-08 23:52:29 +0100

    S_67(651), Finished.

--- state-demo/src/Counter.jsx
+++ state-demo/src/Counter.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 export default function Counter() {
+    console.log('RENDERED!');
     const [count, setCount] = useState(0);
     const addOne = () => {
         setCount(count + 1);
+    };
+    const setToTen = () => {
+        setCount(10);
     };
     const addThree = () => {
         setCount((currentCount) => currentCount + 1);
@@ -17,6 +21,7 @@ export default function Counter() {
             <p>Count:{count}</p>
             <button onClick={addOne}>+1</button>
             <button onClick={addThree}>+3</button>
+            <button onClick={setToTen}>Set To 10</button>
         </div>
     );
 }
