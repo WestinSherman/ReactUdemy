ac982f0de8c491a59cf41363d1c7d80de8cfaffe
WestinSherman <westin@arnst.ca>
2025-03-18 13:58:27 +0100

    S_67(652), Finished.

--- state-demo/src/App.jsx
+++ state-demo/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Counter from './Counter';
 import Dumbo from './Dumbo';
+import ScoreKeeper from './ScoreKeeper';
+import EmojiClicker from './EmojiClicker';
 
 function App() {
 
   return (
     <>
+      <EmojiClicker />
+      {/*<ScoreKeeper />*/}
       {/*<h1>State Demo!</h1>*/}
-      <Counter />
+      {/*<Counter />*/}
       {/*<Dumbo />*/}
     </>
   );
---
+++ state-demo/src/EmojiClicker.jsx
@@ -0,0 +1,16 @@
+import { v4 as uuid } from 'uuid';
+import { useState } from 'react';
+export default function EmojiClicker() {
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😭" }]);
+    const addEmoji = () => {
+        setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: "😎" }]);
+    }
+    return (
+        <div>
+            {emojis.map(e => (
+                <span key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>
+            ))}
+            <button onClick={addEmoji}>Add Emoji</button>
+        </div>
+    );
+}
